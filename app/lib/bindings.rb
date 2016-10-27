4453411d82011aeaa52fb8d3c23b55cbede4ab48
Kyle Newman <kyle@skylenewman.com>
2016-10-27 15:24:42 -0500

    Now binding a style rule to a style element in the default template's head. This impl put scope on head, but you could scope and prop the style element.

--- app/lib/bindings.rb
+++ app/lib/bindings.rb
@@ -1,3 +1,11 @@
 Pakyow::App.bindings do
   # define bindings here
+  scope :head do
+    binding :custom_css do
+      {
+        # Since you're adding this content to the style node, it's content.
+        content: 'body{background-color:#333;background:linear-gradient(#444,#333);/* Dark text on dark background, because we are edgy and stuff.*/}'
+      }
+    end
+  end
 end
--- app/lib/routes.rb
+++ app/lib/routes.rb
@@ -1,6 +1,8 @@
 Pakyow::App.routes do
   default do
     logger.info 'hello'
+    # If you don't bind anything here, the bindings.rb will not be used.
+    view.scope(:head).apply(request) 
   end
 
   # define application routes here
